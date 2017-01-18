8e8916a64807413f0029b938c6f89c1d14f3c76b
Livien LEVERGEOIS <lleverge@student.42.fr>
2017-01-18 11:16:05 +0100

    Added some dir for more clarity + normed

--- includes/shell.h
+++ includes/shell.h
@@ -6,13 +6,48 @@
 /*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 18:43:41 by lleverge          #+#    #+#             */
-/*   Updated: 2017/01/18 09:49:51 by lleverge         ###   ########.fr       */
+/*   Updated: 2017/01/18 11:14:04 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELL_H
 # define SHELL_H
 
+# include "../libft/libft.h"
+# include <sys/types.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/uio.h>
+# include <termios.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <dirent.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <term.h>
 
+typedef struct stat		t_stat;
+typedef struct winsize	t_winsize;
+typedef struct termios	t_termios;
+
+typedef struct		s_cursor
+{
+	int				x;
+	int				y;
+}					t_cursor;
+
+typedef struct		s_term
+{
+	int				fd;
+	t_termios		termios;
+}					t_term;
+
+typedef struct		s_env
+{
+	char			*name;
+	char			*content;
+	struct s_env	*next;
+}					t_env;
 
 #endif
---
+++ sources/lexer/token_sep.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_sep.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/18 10:54:13 by lleverge          #+#    #+#             */
+/*   Updated: 2017/01/18 11:02:13 by lleverge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <shell.h>
+#include <lexer.h>
+
+int			is_sep(char c)
+{
+	return (c == '>' || c == '<' || c == '&' || c == '|');
+}
+
+int			is_and(char *str, int i)
+{
+	str[i + 1] != '&' ? 0 : 1;
+}
+
+int			is_pipe(char *str, int i)
+{
+	str[i + 1] != '|' ? 0 : 1;
+}
+
+int			is_quote(char *str, int i)
+{
+	int		start;
+
+	start = i;
+	while (str[i + 1] != '\'')
+		i++;
+	return (i - start + 2);
+}
+
+int			is_dquote(char *str, int i)
+{
+	int		start;
+
+	start = i;
+	while (str[i + 1] != '\"')
+		i++;
+	return (i - start + 2);
+}
