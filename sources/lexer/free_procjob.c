3f391e0f078cba26c4c3c37de3e49d7caaf029a1
Livien LEVERGEOIS <lleverge@e2r8p18.42.fr>
2017-12-14 14:22:32 +0100

    Added free functions for proc&job lists

---
+++ sources/lexer/free_procjob.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_procjob.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/13 20:27:03 by lleverge          #+#    #+#             */
+/*   Updated: 2017/12/13 22:29:34 by lleverge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <shell.h>
+
+void	destroy_proc_one(t_process *proc)
+{
+	ft_strdel(&proc->cmd);
+//	destroy_fd_list();
+	free(proc);
+	proc = NULL;
+}
+
+void	destroy_proc_list(t_process *proc_list)
+{
+	t_process	*tmp;
+
+	while (proc_list)
+	{
+		tmp = proc_list->next;
+		destroy_proc_one(proc_list);
+		proc_list = tmp;
+	}
+}
+
+void	destroy_job_one(t_job *job)
+{
+	ft_strdel(&job->cmd);
+	destroy_proc_list(job->proc);
+	free(job);
+	job = NULL;
+}
+
+void	destroy_job_list(t_job *job_list)
+{
+	t_job	*tmp;
+
+	while (job_list)
+	{
+		tmp = job_list->next;
+		destroy_job_one(job_list);
+		job_list = tmp;
+	}
+}
