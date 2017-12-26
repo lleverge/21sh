f450f77b32e4814ad59e17fb9d37e1587f5a5fb5
Valentin FROLICH <vfrolich@e2r10p15.42.fr>
2017-12-26 20:01:24 +0100

    make fork great again

--- sources/exec/fork.c
+++ sources/exec/fork.c
@@ -6,7 +6,7 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/24 11:32:12 by lleverge          #+#    #+#             */
-/*   Updated: 2017/12/26 16:22:06 by lleverge         ###   ########.fr       */
+/*   Updated: 2017/12/26 19:55:37 by vfrolich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,17 +53,15 @@ int				exe_fork(t_env *env, t_process *proc, t_ult *ult)
 	if ((pid = fork()) == 0)
 	{
 		signal(SIGINT, SIG_DFL);
+		reset_term(ult->term);
 		if (set_fd_exec(proc) == -1)
 			exit(-1);
-		if (execve(hash_search(*cmd_tab, ult->hash_table),
-			cmd_tab, env_cpy) < 0)
-		{
-			ft_putstr_fd("21sh: execve error", 2);
-			exit(127);
-		}
+		execve(hash_search(*cmd_tab, ult->hash_table), cmd_tab, env_cpy);
 	}
-	wait(&status);
+	else 
+		waitpid(pid, &status, 0);
 	close_fd_exec(proc);
+	init_term();	
 	free_tab(env_cpy);
 	free_tab(cmd_tab);
 	return (status);
