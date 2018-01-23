f56caeeaca91900ea196c42de66fce6c862b16f2
Livien LEVERGEOIS <lleverge@student.42.fr>
2018-01-23 17:58:55 +0100

    Funcs to merge tokens works fine now

--- sources/lexer/lexer2.c
+++ sources/lexer/lexer2.c
@@ -6,132 +6,63 @@
 /*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 12:28:37 by lleverge          #+#    #+#             */
-/*   Updated: 2018/01/23 15:44:37 by lleverge         ###   ########.fr       */
+/*   Updated: 2018/01/23 17:57:22 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lexer.h>
 #include <shell.h>
 
-static t_lexer     *check_merge_aggreg2(t_lexer *lexer)
+static void     check_merge_aggreg2(t_lexer *lexer)
 {
-    t_lexer     *voyager;
-    t_lexer     *tmp;
-    t_lexer     *tmp2;
+	t_lexer		*tmp;
 
-    voyager = lexer;
-    if (voyager->token_id == 12 && voyager->next->token_id == 3 && voyager->next->next
-        && voyager->next->next->token_id < 12)
-	{
-		ft_putendl("21sh: lexer2.c: bad token");
-		exit(-1);
-	}
-    else if (voyager->token_id == 12 && voyager->next->token_id == 3)
-    {
-        tmp = voyager;
-        voyager = voyager->next;
-        tmp2 = voyager->next;
-        ft_strdel(&tmp->content);
-        tmp->content = ft_strdup("&>");
-        tmp->token_id = 11;
-        tmp->next = tmp2;
-        tmp2->prev = tmp;
-        free_lexer(&voyager);
-		lexer = tmp;
-        return (lexer);
-    }
-    else
-        return (lexer);
+	ft_strdel(&lexer->content);
+	lexer->content = ft_strdup("&>");
+	lexer->token_id = 11;
+	tmp = lexer->next->next;
+	lex_free_one(lexer->next);
+	lexer->next = tmp;
+	tmp->prev = lexer;
 }
 
-static t_lexer     *check_merge_aggreg1(t_lexer *lexer)
+static void     check_merge_aggreg1(t_lexer *lexer)
 {
-    t_lexer     *voyager;
-    t_lexer     *tmp;
-    t_lexer     *tmp2;
+	t_lexer     *tmp;
 
-    voyager = lexer;
-    if (voyager->token_id == 3 && voyager->next->token_id == 12 && voyager->next->next
-        && voyager->next->next->token_id < 13)
- 	{
-		ft_putendl("21sh: lexer2.c: bad token");
-		exit(-1);
-	}
-    else if (voyager->token_id == 3 && voyager->next->token_id == 12)
-    {
-        tmp = voyager;
-        voyager= voyager->next;
-        tmp2 = voyager->next;
-        ft_strdel(&tmp->content);
-        tmp->content = ft_strdup(">&");
-        tmp->token_id = 11;
-        tmp->next = tmp2;
-        tmp2->prev = tmp;
-        free_lexer(&voyager);
-		lexer = tmp;
-        return (lexer);
-    }
-    else
-        return (lexer);
+	ft_strdel(&lexer->content);
+	lexer->content = ft_strdup(">&");
+	lexer->token_id = 11;
+	tmp = lexer->next->next;
+	lex_free_one(lexer->next);
+	lexer->next = tmp;
+	tmp->prev = lexer;
 }
 
-static t_lexer		*check_merge_dgreat(t_lexer *lexer)
+static void		check_merge_dgreat(t_lexer *lexer)
 {
-    t_lexer     *voyager;
-    t_lexer     *tmp;
-    t_lexer     *tmp2;
+	t_lexer     *tmp;
 
-    voyager = lexer;
-    if (voyager->token_id == 3 && voyager->next->token_id == 3 && voyager->next->next
-		&& voyager->next->next->token_id < 13)
-	{
-		ft_putendl("21sh: lexer2.c: bad token");
-		exit(-1);
-	}
-    else if (voyager->token_id == 3 && voyager->next->token_id == 3)
-    {
-		tmp = voyager;
-		voyager = voyager->next;
-		tmp2 = voyager->next;
-		ft_strdel(&tmp->content);
-		tmp->content = ft_strdup(">>");
-		tmp->token_id = 4;
-		tmp->next = tmp2;
-		tmp2->prev = tmp;
-		free_lexer(&voyager);
-		lexer = tmp;
-		return (lexer);
-    }
-    else
-		return (lexer);
+	ft_strdel(&lexer->content);
+	lexer->content = ft_strdup(">>");
+	lexer->token_id = 4;
+	tmp = lexer->next->next;
+	lex_free_one(lexer->next);
+	lexer->next = tmp;
+	tmp->prev = lexer;
 }
 
-static t_lexer		*check_merge_dless(t_lexer *lexer)
+static void		check_merge_dless(t_lexer *lexer)
 {
-	t_lexer		*voyager;
-	t_lexer		*tmp2;
+	t_lexer     *tmp;
 
-	voyager = lexer;
-	if (voyager->token_id == 2 && voyager->next->token_id == 2 && voyager->next->next
-		&& voyager->next->next->token_id < 13)
-	{
-		ft_putendl("21sh: lexer2.c: bad token");
-		exit(-1);
-	}
-	else if (voyager->token_id == 2 && voyager->next->token_id == 2)
-	{
-		voyager = voyager->next;
-		tmp2 = voyager->next;
-		ft_strdel(&(lexer->content));
-		lexer->content = ft_strdup("<<");
-		lexer->token_id = 5;
-		lexer->next = tmp2;
-		tmp2->prev = lexer;
-		free_lexer(&voyager);
-		return (lexer);
-	}
-	else
-		return (lexer);
+	ft_strdel(&lexer->content);
+	lexer->content = ft_strdup("<<");
+	lexer->token_id = 5;
+	tmp = lexer->next->next;
+	lex_free_one(lexer->next);
+	lexer->next = tmp;
+	tmp->prev = lexer;
 }
 
 void			group_token(t_lexer **lexer)
@@ -142,13 +73,13 @@ void			group_token(t_lexer **lexer)
 	while (tmp)
 	{
 		if (tmp->token_id == 2 && tmp->next && tmp->next->token_id == 2)
-			tmp = check_merge_dless(tmp);
+			check_merge_dless(tmp);
 		else if (tmp->token_id == 3 && tmp->next && tmp->next->token_id == 3)
-			tmp = check_merge_dgreat(tmp);
+			check_merge_dgreat(tmp);
 		else if (tmp->token_id == 3 && tmp->next && tmp->next->token_id == 12)
-			tmp = check_merge_aggreg1(tmp);
+			check_merge_aggreg1(tmp);
 		else if (tmp->token_id == 12 && tmp->next && tmp->next->token_id == 3)
-			tmp = check_merge_aggreg2(tmp);
+			check_merge_aggreg2(tmp);
 		tmp = tmp->next;
 	}
 }
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 15:50:10 by vfrolich          #+#    #+#             */
-/*   Updated: 2018/01/23 15:41:16 by lleverge         ###   ########.fr       */
+/*   Updated: 2018/01/23 17:55:56 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,8 +119,6 @@ int				init_all(char **environ)
 		if (ult->cmd)
 		{
 			lex = fill_lexer(ult);
-			lex_print(lex);
-			ft_putendl("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 			group_token(&lex);
 			lex_print(lex);
 //			lex ? lex_free_all(lex) : NULL;
