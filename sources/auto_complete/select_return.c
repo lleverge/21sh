f778829e2c91de8c0e41acda4d38b3724a78e520
Livien LEVERGEOIS <lleverge@student.42.fr>
2018-03-27 15:14:55 +0200

    Improved select_return func

--- sources/auto_complete/select_return.c
+++ sources/auto_complete/select_return.c
@@ -6,27 +6,32 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 17:40:52 by lleverge          #+#    #+#             */
-/*   Updated: 2018/03/27 12:17:29 by vfrolich         ###   ########.fr       */
+/*   Updated: 2018/03/27 15:14:11 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <shell.h>
 #include <cmd_edit.h>
 
-void			select_in_prompt(char *select, char *cmd, int index)
+void			select_in_prompt(char *select, t_prompt *prompt)
 {
 	char	*tmp;
+	char	*word;
+	char	*tmp2;
 	char	*result;
 	size_t		i;
 	
 	i = 0;
-	tmp = ft_strdup(cmd + index + 1);
-	ft_memmove(cmd + index + 1, select, ft_strlen(select));
-	result = ft_strcat(cmd, tmp);
+	word = word_detect(prompt);
+	tmp2 = ft_strstr(word);
+	tmp = ft_strdup(prompt->cmd + prompt->i + );
+	ft_memmove(prompt->cmd + tmp2, select, ft_strlen(select));
+	result = ft_strcat(prompt->cmd, tmp);
 	while (i < ft_strlen(result))
 	{
-		cmd[i] = result[i];
+		prompt->cmd[i] = result[i];
 		i++;
 	}
 	ft_strdel(&tmp);
+	ft_strdel(word);
 }
