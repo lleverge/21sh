14332b74c8d89a38c87d6dc8cf8a00073e4976ac
Valentin FROLICH <vfrolich@e2r9p20.42.fr>
2018-04-11 19:42:51 +0200

    redirection and pipe fd conflict resolved

--- sources/exec/apply_redirect.c
+++ sources/exec/apply_redirect.c
@@ -6,27 +6,13 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 19:01:05 by vfrolich          #+#    #+#             */
-/*   Updated: 2018/03/31 14:36:08 by vfrolich         ###   ########.fr       */
+/*   Updated: 2018/04/11 19:30:00 by vfrolich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cmd_edit.h>
 #include <shell.h>
 #include <lexer.h>
-#include <stdio.h>
-
-void			set_fd_pipe(t_process *proc_1, t_process *proc_2)
-{
-	int		fd[2];
-
-	if (pipe(fd) == -1)
-	{
-		ft_putendl_fd("21sh: pipe error", 2);
-		exit(1);
-	}
-	proc_1->fd[1] = fd[1];
-	proc_2->fd[0] = fd[0];
-}
 
 t_job			*apply_redirect(t_job *job_li, t_ult *ult)
 {
@@ -44,46 +30,4 @@ t_job			*apply_redirect(t_job *job_li, t_ult *ult)
 		tmp = tmp->next;
 	}
 	return (job_li);
-}
-
-static void		simple_sigint(int signal)
-{
-	if (signal != SIGINT)
-		return ;
-	ft_putchar('\n');
-}
-
-void			job_launch(t_job *job_li, t_ult *ult)
-{
-	char		**cmd_tab;
-	t_job		*tmp_job;
-	t_process	*tmp_proc;
-	int			fd[2];
-
-	cmd_tab = NULL;
-	tmp_job = job_li;
-	signal(SIGINT, &simple_sigint);
-	while (tmp_job)
-	{
-		tmp_job = apply_redirect(tmp_job, ult);
-		tmp_proc = tmp_job->proc;
-		while (tmp_proc)
-		{
-			cmd_tab = ft_whitespace(tmp_proc->cmd);
-			if (tmp_proc->next)
-			{
-				pipe(fd);
-				tmp_proc->fd[1] = fd[1];
-			}
-			ult->ret = seek_and_exec(ult, tmp_proc, cmd_tab, fd);
-			if (tmp_proc->next)
-				tmp_proc->next->fd[0] = fd[0];
-			cmd_tab ? free_tab(cmd_tab) : NULL;
-			tmp_proc = tmp_proc->next;
-		}
-		wait_for_procs(tmp_job->proc);
-		tmp_job = tmp_job->next;
-	}
-	free(ult->term);
-	ult->term = init_term();
 }
---
+++ sources/exec/job_launch.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   job_launch.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/11 19:28:46 by vfrolich          #+#    #+#             */
+/*   Updated: 2018/04/11 19:41:32 by vfrolich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cmd_edit.h>
+#include <shell.h>
+#include <lexer.h>
+
+static void		simple_sigint(int signal)
+{
+	if (signal != SIGINT)
+		return ;
+	ft_putchar('\n');
+}
+
+void			proc_launch(t_process *proc, t_ult *ult, int fd[2])
+{
+	t_process	*tmp_proc;
+	char		**cmd_tab;
+
+	cmd_tab = NULL;
+	tmp_proc = proc;
+	while (tmp_proc)
+	{
+		cmd_tab = ft_whitespace(tmp_proc->cmd);
+		if (tmp_proc->next)
+		{
+			pipe(fd);
+			if (tmp_proc->fd[1] == 1)
+				tmp_proc->fd[1] = fd[1];
+			else
+				close(fd[1]);
+		}
+		ult->ret = seek_and_exec(ult, tmp_proc, cmd_tab, fd);
+		if (tmp_proc->next)
+			tmp_proc->next->fd[0] = fd[0];
+		cmd_tab ? free_tab(cmd_tab) : NULL;
+		tmp_proc = tmp_proc->next;
+	}
+}
+
+void			job_launch(t_job *job_li, t_ult *ult)
+{
+	t_job		*tmp_job;
+	int			fd[2];
+
+	tmp_job = job_li;
+	signal(SIGINT, &simple_sigint);
+	while (tmp_job)
+	{
+		tmp_job = apply_redirect(tmp_job, ult);
+		proc_launch(tmp_job->proc, ult, fd);
+		wait_for_procs(tmp_job->proc);
+		tmp_job = tmp_job->next;
+	}
+	free(ult->term);
+	ult->term = init_term();
+}
