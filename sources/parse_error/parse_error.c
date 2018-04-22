c85b1fbb1c9e5f23c88b76a935e9f2c3d6ea2e22
Livien LEVERGEOIS <lleverge@student.42.fr>
2018-04-22 20:08:01 +0200

    Fixed parser again when quote with multi lines

--- sources/parse_error/parse_error.c
+++ sources/parse_error/parse_error.c
@@ -6,7 +6,7 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 18:17:33 by lleverge          #+#    #+#             */
-/*   Updated: 2018/04/22 17:42:42 by lleverge         ###   ########.fr       */
+/*   Updated: 2018/04/22 20:07:23 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,8 +82,8 @@ static int		parse_separator(t_lexer *lex)
 	t_lexer *tmp;
 
 	tmp = lex;
-	if ((tmp->token_id == SEPARATOR && !LNEXT) || (tmp->token_id && LNEXT
-		&& !LDNEXT && is_full_spaces(LNEXT->content)))
+	if ((tmp->token_id == SEPARATOR && !LNEXT) || (tmp->token_id == SEPARATOR
+		&& LNEXT && !LDNEXT && is_full_spaces(LNEXT->content)))
 		return (1);
 	if (tmp->token_id == SEPARATOR && LNEXT && (LNEXT->token_id == SEPARATOR ||
 	LNEXT->token_id == LESS || LNEXT->token_id == GREAT || LNEXT->token_id
