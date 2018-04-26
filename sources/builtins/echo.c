7707e90b083b7afbb4a9236ccc98f5feac47ac1e
Valentin FROLICH <vfrolich@e1r8p21.42.fr>
2018-04-26 22:53:30 +0200

    fixed echo

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -6,33 +6,48 @@
 /*   By: vfrolich <vfrolich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 16:02:23 by vfrolich          #+#    #+#             */
-/*   Updated: 2018/04/23 21:19:12 by vfrolich         ###   ########.fr       */
+/*   Updated: 2018/04/26 20:30:15 by vfrolich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
 
-int		ft_echo(char **str)
+static int		no_nl(char **arg)
 {
+	int		i;
+
+	i = 0;
+	while (arg[i])
+	{
+		ft_putstr(arg[i]);
+		i++;
+		if (arg[i])
+			ft_putchar(32);
+	}
+	return (0);
+}
+
+int				ft_echo(char **str)
+{
+	int		i;
+
 	if (*str && !ft_strcmp(*str, "-n"))
 	{
 		str++;
-		while (*str)
-		{
-			ft_putstr(*str);
-			str++;
-		}
-		return (0);
+		return (no_nl(str));
 	}
 	if (*str && !ft_strncmp(*str, "-n", 2) && ft_strlen(*str) > 2)
 	{
 		ft_putendl_fd("usage: echo [-n] [string]", 2);
 		return (1);
 	}
-	while (*str)
+	i = 0;
+	while (str[i])
 	{
-		ft_putstr(*str);
-		str++;
+		ft_putstr(str[i]);
+		i++;
+		if (str[i])
+			ft_putchar(32);
 	}
 	ft_putchar('\n');
 	return (0);
